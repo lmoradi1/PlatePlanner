5af44a5b35922d3f48621063b0c60d239a736496
almalinux Cloud User <student@labSDANZF.5xyb12hqwafubchsk0krzmscbf.bx.internal.cloudapp.net>
2025-04-10 15:07:59 +0000

    Added preferences model, added filters to preferences page

--- project/models.py
+++ project/models.py
@@ -1,12 +1,13 @@
 from flask_login import UserMixin
 from . import db
 
-class User(UserMixin,db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     favorites = db.relationship('FavoriteRecipe', backref='user', lazy=True)
+    preferences = db.relationship('UserPreferences', backref='user', uselist=False)
 
 class FavoriteRecipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,3 +15,13 @@ class FavoriteRecipe(db.Model):
     title = db.Column(db.String(255))
     image = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class UserPreferences(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
+    gluten_free = db.Column(db.Boolean, default=False)
+    dairy_free = db.Column(db.Boolean, default=False)
+    soy_free = db.Column(db.Boolean, default=False)
+    egg_free = db.Column(db.Boolean, default=False)
+    nut_free = db.Column(db.Boolean, default=False)
+    sugar_free = db.Column(db.Boolean, default=False)
